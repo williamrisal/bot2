47d37b83c046ff48493fad04044a2ffc15eb3881
William risal <63760727+williamrisal@users.noreply.github.com>
2021-05-30 15:55:28 +0200

    Add files via upload

--- screenshot.js
+++ screenshot.js
@@ -2,15 +2,15 @@ const puppeteer = require("puppeteer")
 const num = '0699766246'
 const { exec } = require("child_process");
 var d = new Date();
-var hours = d.getHours();
-var second = d.getSeconds();
-var minute = d.getMinutes();
 command = "./pushbullet.sh Crédit '"
 
 const getData = async () =>{
   text = ""
   while (1){
-    const browser = await puppeteer.launch({ headless: true })
+    var hours = d.getHours();
+    var second = d.getSeconds();
+    var minute = d.getMinutes();
+    const browser = await puppeteer.launch({ headless: true, slowMo: 150})
     const page = await browser.newPage()
     await page.goto("https://intra.epitech.eu/auth-1bb7d152410cf813bb1df7d5a5b80e24eceac31e")
     const selector_body = await page.$x("/html/body/div[1]/div[2]/div/div[1]/div[3]/div[1]/span[1]");
